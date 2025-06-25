c09281fce71a751c2d2e4500020562d588d5e1a5
Nischal Mainali <nischalmainali75@gmail.com>
2025-06-25 14:29:05 +0545

    movie api

--- index.js
+++ index.js
@@ -8,6 +8,16 @@ const indexRouter=require("./Routes/index")
 const Port=Number(process.env.Port)
 app.use(express.json())
 
+
+app.use((req,res,next)=>{
+    if (!req.body) req.body = {};
+  req.body.country="us"
+  req.body.currency="usd"
+  req.body.currentTime= new Date().toISOString()
+
+next();
+})
+
 app.use("/",indexRouter);
 
 app.use((err, req, res, next) => {
