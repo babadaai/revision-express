340d06e28861111d8865a00249dfcc69ff3b9dc0
Nischal Mainali <nischalmainali75@gmail.com>
2025-07-12 13:01:55 +0545

    12 july

--- index.js
+++ index.js
@@ -2,21 +2,15 @@ require("dotenv").config()
 
 const express =require(`express`)
 const app =express();
+const morgan=require("morgan")
 
 const indexRouter=require("./Routes/index")
 
 const Port=Number(process.env.Port)
 app.use(express.json())
 
-
-app.use((req,res,next)=>{
-    if (!req.body) req.body = {};
-  req.body.country="us"
-  req.body.currency="usd"
-  req.body.currentTime= new Date().toISOString()
-
-next();
-})
+app.use("/assets",express.static("public"))
+app.use(morgan("dev"))
 
 app.use("/",indexRouter);
 
